7639f8ef0f8416035685ad650969c607c09dc00d
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2025-01-10 13:31:43 +0530

    build delete card functionality

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import User from '../User/User'
 import userData from './UserData'
 import NavbarComp from './Navbar/Navbar'
@@ -6,21 +6,31 @@ import NavbarComp from './Navbar/Navbar'
 
 const  Dashboard = ({logout}) => {
 
-// console.log(userdata);
+  const [user,setUser]=useState(userData);
+
+
+  function onUserDelete(id){
+    console.log("User Deleted with user id as: ",id);
+
+    const updatedUser=user.filter((user)=>{
+      return user.id!=id;
+    })
+    setUser(updatedUser);
+    
+  }
 
   return (
     <div className='bg-gray-600'>
       <NavbarComp logouts={logout}/>
       <div className='flex flex-wrap justify-center align-center'>
         {
-          userData.map((users)=>{
+          user.map((users)=>{
             return (
-              <User key="" userData={users}/>
+              <User click={onUserDelete} key="" userData={users}/>
             )
           })
         }
-      </div>
-      
+      </div>      
     </div>
   )
 }
--- src/User/User.jsx
+++ src/User/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import userData from '../Components/UserData'
+import "./User.css"
 
 
 const User = (props) => {
@@ -7,11 +8,12 @@ const User = (props) => {
     const datas=props.userData;
     
   return (
-    <div className='rounded-2xl text-2xl p-3 m-4 bg-green-300 border flex flex-col justify-center items-center'>
+    <div className='rounded-2xl text-2xl p-3 m-4 bg-green-300 border flex flex-col justify-center items-center position: relative'>
         <img className='h-52' src={datas.image} alt=""/>
         <h1>{datas.firstName, datas.lastName}</h1>
         <p>{datas.email}</p>
-        <p>{datas.phone}</p>       
+        <p>{datas.phone}</p> 
+        <span onClick={()=> props.click(datas.id)} className='position: absolute top-2 right-4 cursor: cursor-pointer bg-white px-2 py-1 rounded-xl'> X </span>      
     </div>
   )
 }
